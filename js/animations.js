6b183f169cc1c4d50660a6d32bdd65f59bc8a571
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-05-06 19:38:23 -0400

    out on the modal

--- js/animations.js
+++ js/animations.js
@@ -3,16 +3,4 @@ $(document).ready(function () {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
-
-  $("div.signup-button").click(function () {
-    console.log("modal");
-    $(".modal").toggleClass("is-active");
-    $(".modal").toggleClass("is-clipped");
-  });
-
-  $("button.close-modal").click(function () {
-    console.log("modal");
-    $(".modal").toggleClass("is-active");
-    $(".modal").toggleClass("is-clipped");
-  });
 });
